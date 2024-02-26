f66a0cda90aa96026b68664ef9b184209a49e9b1
yonghuname <89337531+yonghuname@users.noreply.github.com>
2024-02-27 03:33:53 +0800

    修复了error问题的bug，也修复404的跳转问题

--- src/main/java/com/eoft/blog2/handler/ControllerExceptionHandler.java
+++ src/main/java/com/eoft/blog2/handler/ControllerExceptionHandler.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 import org.slf4j.Logger;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
 import java.util.Optional;
 
 
@@ -28,7 +30,7 @@ public class ControllerExceptionHandler {
      */
     @ExceptionHandler({Exception.class})
     public ModelAndView handleException(HttpServletRequest request, Exception e) throws Exception {
-
+        System.out.println("handleException被调用----------");
         logger.error("Request URL:{},Exception:{}", request.getRequestURL(), e);
 
         if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) !=null) {
@@ -38,15 +40,19 @@ public class ControllerExceptionHandler {
         // 如果异常已经被标记为某个特定的HTTP响应状态，直接抛出
         ModelAndView mav = new ModelAndView();
         mav.addObject("url", request.getRequestURL());
-/*
-        if (e instanceof ChangeSetPersister.NotFoundException) {
+
+        if (e instanceof NoFoundException) {
             mav.setViewName("error/404");
-        }  else {
+        }  else if (e instanceof NoResourceFoundException){
+            mav.setViewName("error/404");
+
+        }
+            else {
             mav.setViewName("error/error");
         }
-        */
 
-        mav.setViewName("error/error");
+
+//        mav.setViewName("error/error");
         mav.addObject("exception", e);
         return mav;
     }
