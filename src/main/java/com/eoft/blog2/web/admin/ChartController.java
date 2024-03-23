48986ed60ef45084adf6ee859e49cd4aa2fedd91
yonghuname <etgituse@163.com>
2024-03-23 23:49:49 +0800

    图表

--- src/main/java/com/eoft/blog2/web/admin/ChartController.java
+++ src/main/java/com/eoft/blog2/web/admin/ChartController.java
@@ -1,18 +1,29 @@
 package com.eoft.blog2.web.admin;
 
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/admin")
 public class ChartController {
-
-    @GetMapping("/chart")
-    public String getchart(){
-
-        return "admin/chart";
+    @GetMapping("/chart22")// 先废弃
+        public String getchart(){
+        return "/admin/chart";
     }
 
 
+    @GetMapping("/chart")
+     public ResponseEntity<String>  getchart1() throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
+        String htmlContent = restTemplate.getForObject("http://118.31.237.220:889/", String.class);
+
+        return new ResponseEntity<>(htmlContent, HttpStatus.OK);
+    }
 }
